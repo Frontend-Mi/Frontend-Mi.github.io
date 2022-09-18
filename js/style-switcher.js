8c49c23dbadad90ebc8c187eecce2bb45f1c4c97
Frontend-Mi <113367112+Frontend-Mi@users.noreply.github.com>
2022-09-19 01:14:57 +0700

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -43,3 +43,25 @@ window.addEventListener("load", () => {
         dayNight.querySelector("i").classList.add("fa-moon");
     }
 })
+
+// Fullscreen
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||  
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
